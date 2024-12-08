5d4ac2701c04eda52d03567b4b962a36a7ec5bbf
rohit99504 <163815811+rohit99504@users.noreply.github.com>
2024-12-08 08:35:01 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -138,7 +138,7 @@ def old_download(url, file_name, chunk_size = 1024 * 10):
 
 def human_readable_size(size, decimal_places=2):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
-        if size < 1024.0 or unit == 'KB':
+        if size < 1024.0 or unit == 'B':
             break
         size /= 1024.0
     return f"{size:.{decimal_places}f} {unit}"
